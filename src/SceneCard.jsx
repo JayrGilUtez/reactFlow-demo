e37b562a59e49dae77f135b4211e5b3f1059876a
JayrGil <20223tn052@utez.edu.mx>
2024-05-24 14:13:22 -0500

    feat(components) SceneCard component added

---
+++ src/SceneCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+export default function SceneCard() {
+    return (
+        <div className='sceneCard' >
+            <h4>Title</h4>
+            <div
+                style={{
+                    backgroundColor: '#d0d0d0',
+                    height: 200,
+                    width: 350,
+                    borderRadius: 5,
+                    alignContent: 'center'
+
+                }}
+            >
+                CONTENT
+
+            </div>
+
+            <div style={
+                {
+                    width: 400,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    alignSelf: 'flex-end'
+                    
+                }
+            }
+            >
+
+                <div className='optionButton'> A </div>
+                <div className='optionButton'> B </div>
+
+            </div>
+
+        </div>
+    )
+}
+
