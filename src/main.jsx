d3b7a57037ceee50d4c932b99f7de24e42144cbb
JayrGil <20223tn052@utez.edu.mx>
2024-05-29 08:16:06 -0500

    now stories manager component is beeing rendered in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 import Flow from './Flow.jsx'
+import StoriesManager from './components/stories/StoriesManager.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Flow />
+    <StoriesManager />
   </React.StrictMode>,
 )
