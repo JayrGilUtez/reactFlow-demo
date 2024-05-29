9b67cade72f57a5ff67ab91753cae0bba71316ee
JayrGil <20223tn052@utez.edu.mx>
2024-05-29 13:15:54 -0500

    feat(API) fetchData() function added to get all stories of a specified user

--- src/components/stories/StoriesManager.jsx
+++ src/components/stories/StoriesManager.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './storiesManagerStyles.css';
 import { Link } from 'react-router-dom';
 import StoryCard from './StoryCard.jsx';
+import AxiosClient from '../../config/http-client/axios-client.js';
 
 export default function StoriesManager() {
+    const [stories, setStories] = useState([]);
+    // La constante user_id es temporal, user_id se
+    // obtendra de la sesion del usuario o por medio de props
+    const user_id = 1;
+    
+    useEffect(() => {
+        async function fetchData() {
+            try {
+
+                //Get story by story_id ->  http://localhost:3000/story/4
+                //Get stories by user_id ->  http://localhost:3000/stories/1 
+
+                const response = await AxiosClient({
+                    url: `/stories/${user_id}`,
+                    method: 'GET',
+                });
+                setStories(response);
+                console.log(response);
+            } catch (error) {
+                console.error('There was an error!', error);
+            }
+        }
+
+        fetchData();
+
+    }, []);
+
 
     return (
         <div className='stories-container' >
-            <Link to={'/stories'}>
-                <StoryCard data={{ title: 'Titulo' }} />
 
-            </Link>
-
-            <StoryCard />
-            <StoryCard />
-            <StoryCard />
-            <StoryCard />
-            <StoryCard />
-            <StoryCard />
-            <StoryCard />
+            {stories && stories.map((story, index) => (
+                <Link key={index} to={`story/${story.story_id}`}>
+                    <StoryCard data={story} />
+                </Link>
+            ))}
 
         </div>
     )
