2dd7660a77f1dfd3225b1228864f2f8849eb6057
JayrGil <20223tn052@utez.edu.mx>
2024-05-31 15:25:59 -0500

    feat(sidbar) FilesUploader component implemented in sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-
+import FilesUploader from './components/files-uploader/FilesUploader.jsx';
 export default () => {
   // Para arrastrar objetos del sidebar
   const onDragStart = (event, nodeType) => {
@@ -9,13 +9,17 @@ export default () => {
   };
 
   return (
-    <aside style={{ backgroundColor: '#1e1e1e', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <aside style={{ backgroundColor: '#1e1e1e', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div className="description">
         <h3>Herramientas</h3>
       </div>
       <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'customNode')} draggable>
-        Nueva escena 
+        Nueva escena
       </div>
+      <FilesUploader/>
+
+
+
       
     </aside>
   );
